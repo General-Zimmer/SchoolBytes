15f079464eaf5f813fb14eeb2b47e002f28d2c2f
Denduff <Andreas.elbyhoej@live.dk>
2024-10-31 22:08:14 +0100

    messed up during saving so it was incomplete. Completed the init models

--- SchoolBytes/SchoolBytes/models/CourseModule.cs
+++ SchoolBytes/SchoolBytes/models/CourseModule.cs
@@ -5,6 +5,7 @@ public class CourseModule : IModule
 {
     public string Name { get; set; }
     public Teacher Teacher { get; set; }
+    public FoodModule FoodModule { get; set; }
     public DateTime Date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public DateTime StartTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public DateTime EndTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
