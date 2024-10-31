891e267404379cf3cf3021cd39af60f18c523a90
Denduff <Andreas.elbyhoej@live.dk>
2024-10-31 21:26:33 +0100

    added initial models

---
+++ SchoolBytes/SchoolBytes/models/CourseModule.cs
@@ -0,0 +1,11 @@
+﻿using SchoolBytes.Models;
+using System;
+
+public class CourseModule : IModule
+{
+    public string Name { get; set; }
+    public Teacher Teacher { get; set; }
+    public DateTime Date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime StartTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime EndTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
