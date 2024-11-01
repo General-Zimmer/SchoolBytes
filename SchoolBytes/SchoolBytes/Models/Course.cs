85eed8330e8116603520f134bd2ef5ba01f5c0f2
kaps <kasperjonassen@yahoo.dk>
2024-11-01 12:54:16 +0100

    added CourseController.cs, modified Course.cs

--- SchoolBytes/SchoolBytes/Models/Course.cs
+++ SchoolBytes/SchoolBytes/Models/Course.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 
 public class Course
 {
+
     public string Name { get; set; }
     public string Description { get; set; }
     public Teacher Teacher { get; set; }
-    public List<Participant> Participants { get; set; }
-    public List<CourseModule> Courses { get; set; }
+    public List<Participant> Participants { get; set; } = new List<Participant>();
+    public List<CourseModule> Courses { get; set; } = new List<CourseModule>();
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public int MaxCapacity { get; set; }
+    public int Id { get; set; }
+
+    public Course(string name, string description, DateTime startDate, DateTime endDate, int maxCapacity)
+    {
+        Name = name;
+        Description = description;
+        StartDate = startDate;
+        EndDate = endDate;
+        MaxCapacity = maxCapacity;
+    }
 
 }
