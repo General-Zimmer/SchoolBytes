1730fa27f037a31a98631cc863fa1b314ba2b048
Denduff <Andreas.elbyhoej@live.dk>
2024-11-01 12:28:12 +0100

    readded models etc

---
+++ SchoolBytes/SchoolBytes/Models/Models/IModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBytes.Models
+{
+    internal interface IModule
+    {
+        string Name { get; set; }
+        int Capacity { get; set; }
+        DateTime Date { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        Course Course { get; set; }
+        Teacher Teacher { get; set; }
+        //Consider making location a class or enum so we can have static locations
+        string Location { get; set; }
+    }
+}
---
+++ SchoolBytes/SchoolBytes/Models/Models/Teacher.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public class Teacher
+{
+    public string Name { get; set; }
+    public List<Course> Courses { get; set; }
+}
