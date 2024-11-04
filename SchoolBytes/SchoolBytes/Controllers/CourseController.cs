f320b7f7d53ad1d006b4927c815ba9ea376591f0
Duffy <and@digital-identity.dk>
2024-11-04 11:36:01 +0100

    made courses always return the same list of courses

--- SchoolBytes/SchoolBytes/Controllers/CourseController.cs
+++ SchoolBytes/SchoolBytes/Controllers/CourseController.cs
@@ -7,7 +7,44 @@ namespace SchoolBytes.Controllers
 {
     public class CourseController : Controller
     {
-        private List<Course> courses = new List<Course>();
+        private List<Course> courses {
+            get
+            {
+                var teacher1 = new Teacher { Name = "John Doe" };
+                var participant1 = new Participant { Name = "Alice" };
+                var participant2 = new Participant { Name = "Bob" };
+
+                var course1 = new Course(
+                    "Science 101",
+                    "A foundational course in basic scientific principles",
+                    teacher1,
+                    DateTime.Now,
+                    DateTime.Now.AddMonths(1).AddDays(1),
+                    25,
+                    1
+                );
+                course1.Participants.Add(participant1);
+                course1.Participants.Add(participant2);
+
+                var course2 = new Course(
+                    "Math 101",
+                    "A foundational course in basic math principles",
+                    teacher1,
+                    DateTime.Now.AddDays(1),
+                    DateTime.Now.AddMonths(1).AddDays(1),
+                    25,
+                    2
+
+                );
+                var courseTest = new List<Course>();
+                courseTest.Add(course1);
+                courseTest.Add(course2);
+                return courseTest;
+            }
+            set
+            {
+
+            } }
 
         //Initial test data
         public void TestData()
