53be6ddc84a931e955795630652b49168ba7f8b1
Remy <rumena.mp@gmail.com>
2024-11-08 11:59:53 +0100

    branch 30 WIP

--- SchoolBytes/SchoolBytes/Controllers/CourseController.cs
+++ SchoolBytes/SchoolBytes/Controllers/CourseController.cs
@@ -15,6 +15,8 @@ namespace SchoolBytes.Controllers
                 var participant1 = new Participant { Name = "Alice" };
                 var participant2 = new Participant { Name = "Bob" };
 
+                
+
                 var course1 = new Course(
                     "Science 101",
                     "A foundational course in basic scientific principles",
@@ -24,8 +26,56 @@ namespace SchoolBytes.Controllers
                     25,
                     1
                 );
+
+                // Create dummy CourseModule objects with fully initialized properties
+                var courseModule1 = new CourseModule
+                {
+                    Id = 1,
+                    Name = "Module 1",
+                    Teacher = teacher1,
+                    FoodModule = null,
+                    Date = DateTime.Now,
+                    StartTime = new DateTime(2024, 11, 10, 9, 0, 0), // 9:00 AM
+                    EndTime = new DateTime(2024, 11, 10, 12, 0, 0),  // 12:00 PM
+                    Capacity = 20,
+                    Course = course1,
+                    Location = "Room 101"
+                };
+
+                var courseModule2 = new CourseModule
+                {
+                    Id = 2,
+                    Name = "Module 2",
+                    Teacher = teacher1,
+                    FoodModule = null,
+                    Date = DateTime.Now.AddDays(7),
+                    StartTime = new DateTime(2024, 11, 11, 13, 0, 0), // 1:00 PM
+                    EndTime = new DateTime(2024, 11, 11, 16, 0, 0),  // 4:00 PM
+                    Capacity = 25,
+                    Course = course1,
+                    Location = "Room 102"
+                };
+
+                var courseModule3 = new CourseModule
+                {
+                    Id = 3,
+                    Name = "Module 3",
+                    Teacher = teacher1,
+                    FoodModule = null,
+                    Date = DateTime.Now.AddDays(14),
+                    StartTime = new DateTime(2024, 11, 12, 10, 0, 0), // 10:00 AM
+                    EndTime = new DateTime(2024, 11, 12, 13, 0, 0),  // 1:00 PM
+                    Capacity = 15,
+                    Course = course1,
+                    Location = "Room 103"
+                };
+
+
                 course1.Participants.Add(participant1);
                 course1.Participants.Add(participant2);
+                course1.Courses.Add(courseModule1);
+                course1.Courses.Add(courseModule2);
+                course1.Courses.Add(courseModule3);
 
                 var course2 = new Course(
                     "Math 101",
@@ -75,6 +125,21 @@ namespace SchoolBytes.Controllers
 
             return View(course);
         }
+
+        // GET: course/{id}/modules (Get course modules by course ID)
+        [HttpGet]
+        [Route("course/{id}/ModulesOverview")]
+        public ActionResult ModulesOverview(int id)
+        {
+            var course = courses.FirstOrDefault(x => x.Id == id);
+            if (course == null)
+            {
+                return HttpNotFound("Course not found");
+            }
+
+            return View(course.Courses); // Passes only the course modules to the view
+        }
+
 
         // POST: api/course/{id} (Update course)
         [HttpPost]
