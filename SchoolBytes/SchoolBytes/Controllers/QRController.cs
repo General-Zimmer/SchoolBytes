04204e0a4c1e8be7e296604f2685d4a8d5c3e52e
Jakob Wolf <jakob-wolf14@live.dk>
2024-11-11 21:43:06 +0100

    Task #13 Har delvist lavet en fremmøde tjek ind side hvor meningen er at man kan vælge course -> module, indtaste tlf nr og så tjekke ind, mangler stadig noget logik og en modul vælger

--- SchoolBytes/SchoolBytes/Controllers/QRController.cs
+++ SchoolBytes/SchoolBytes/Controllers/QRController.cs
@@ -7,15 +7,19 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Web;
-
+using System.Data.Common;
+using SchoolBytes.DTO;
+using System.Linq;
 
 namespace SchoolBytes.Controllers
 {
     public class QRController : Controller
     {
+        DBConnection dbConnection = DBConnection.getDBContext();
+
         readonly QRGeneratorService generatorService = new QRGeneratorService();
         [HttpGet]
-        public ActionResult GetQRImage()
+        public ActionResult GenerateQR()
         {
             string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority;
             string fullUrl = baseUrl + "/QR/Index";
@@ -27,8 +31,9 @@ namespace SchoolBytes.Controllers
                 byte[] imageBytes = ms.ToArray();
                 base64String = Convert.ToBase64String(imageBytes);
             }
-            ViewBag.Message = "The QR brought you here but i will bring you salvation.";
-            ViewBag.Title = "QR Generator";
+            ViewBag.Message = "På denne side kan du genere en ny QR kode.\n" +
+                "Når en ny kode er lavet virker de tidligere ikke.";
+            ViewBag.Title = "QR Kode Oprettelse";
             ViewBag.ImageData = base64String;
             return View();
         }
@@ -36,8 +41,36 @@ namespace SchoolBytes.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Title = "Fremmøde Tjek Ind";
+            return View(dbConnection.courses.ToList());
+        }
 
-            return View();
+        // GET: Course
+        [HttpGet]
+        //[Route("QR")]
+        public ActionResult CourseOverview(int? selectedCourseId = null)
+        {
+            if (selectedCourseId != null)
+            {
+                ViewBag.SelectedCourseId = selectedCourseId;
+            }
+
+            return View(dbConnection.courses.ToList());
+        }
+
+
+        // GET: course/{id}/modules (Get course modules by course ID)
+        [HttpGet]
+        //[Route("course/{id}/moduleOverview")]
+        public ActionResult ModuleOverview(int id)
+        {
+            var course = dbConnection.courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound("Course not found");
+            }
+
+            return View(course.CoursesModules.ToList()); // Passes only the course modules to the view
         }
     }
 }
