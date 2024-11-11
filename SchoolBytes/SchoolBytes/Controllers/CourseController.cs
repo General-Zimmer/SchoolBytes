0f3afc1762cc7020b521cb80a0ac6fbae3e76771
kaps <kasperjonassen@yahoo.dk>
2024-11-11 10:15:51 +0100

    teachers fixed

--- SchoolBytes/SchoolBytes/Controllers/CourseController.cs
+++ SchoolBytes/SchoolBytes/Controllers/CourseController.cs
@@ -10,49 +10,22 @@ namespace SchoolBytes.Controllers
 {
     public class CourseController : Controller
     {
-        private List<Course> courses
-        {
-            get
-            {
-                var teacher1 = new Teacher { Name = "John Doe" };
-                var participant1 = new Participant { Name = "Alice" };
-                var participant2 = new Participant { Name = "Bob" };
-
-                var course1 = new Course(
-                    "Science 101",
-                    "A foundational course in basic scientific principles",
-                    teacher1,
-                    DateTime.Now,
-                    DateTime.Now.AddMonths(1).AddDays(1),
-                    25,
-                    1
-                );
-                course1.Participants.Add(participant1);
-                course1.Participants.Add(participant2);
-
-                var course2 = new Course(
-                    "Math 101",
-                    "A foundational course in basic math principles",
-                    teacher1,
-                    DateTime.Now.AddDays(1),
-                    DateTime.Now.AddMonths(1).AddDays(1),
-                    25,
-                    2
-
-                );
-                var courseTest = new List<Course>();
-                courseTest.Add(course1);
-                courseTest.Add(course2);
-                return courseTest;
-            }
-            set { }
-        }
+        DBConnection dbConnection = DBConnection.getDBContext();
 
         // GET: Course
-        public ActionResult Index()
+        [HttpGet]
+        [Route("course")]
+
+        public ActionResult CourseOverview(int? selectedCourseId = null)
         {
-            return RedirectToAction("CourseOverview");
+            if(selectedCourseId != null)
+            {
+                ViewBag.SelectedCourseId = selectedCourseId;
+            }
+
+            return View(dbConnection.courses.ToList());
         }
+      
 
         // POST: api/course (Add new course)
         [HttpPost]
@@ -124,31 +97,36 @@ namespace SchoolBytes.Controllers
                 remainingModules--;
             }
             
-            course.CoursesModules = modules;
-            courses.Add(course);
+            course.CoursesModules.AddRange(modules);
+            dbConnection.Add(course);
+
+            dbConnection.SaveChanges();
+            ViewBag.teachers = dbConnection.teachers;
             return RedirectToAction("CourseOverview");
         }
-
+     
         // GET: api/course/{id} (Get course by ID)
         [Route("course/{id}")]
         public ActionResult GetCourse(int id)
         {
-            // var course = courses.SingleOrDefault(c => c.Id == id);
-            var course = courses.FirstOrDefault(x => x.Id == id);
+            Course course = dbConnection.courses.Find(id);
+
             if (course == null)
             {
                 return HttpNotFound("Course not found");
             }
-            
-            return View(course);
-        } 
 
-        // POST: api/course/{id} (Update course)
+            return View(course);
+        }
+
+        // POST: api/course/update/{id} (Update course)
         [HttpPost]
         [Route("course/update/{id}")]
         public ActionResult Update(int id, Course updatedCourse)
         {
-            var course = courses.FirstOrDefault(x => x.Id == id);
+            
+            Course course = dbConnection.courses.Find(id);
+           
             if (course == null)
             {
                 return HttpNotFound("Course not found");
@@ -163,10 +141,17 @@ namespace SchoolBytes.Controllers
                 course.EndDate = updatedCourse.EndDate;
                 course.MaxCapacity = updatedCourse.MaxCapacity;
 
+                    course.Name = updatedCourse.Name;
+                    course.Description = updatedCourse.Description;
+                    course.StartDate = updatedCourse.StartDate;
+                    course.EndDate = updatedCourse.EndDate;
+                    course.MaxCapacity = updatedCourse.MaxCapacity;
+                    
+                dbConnection.SaveChanges();
+
                 return RedirectToAction("CourseOverview");
             }
 
-            ViewBag.Teacker
             return View(course);
         }
 
@@ -175,22 +160,23 @@ namespace SchoolBytes.Controllers
         [Route("course/delete/{id}")]
         public ActionResult Delete(int id)
         {
-            var course = courses.SingleOrDefault(c => c.Id == id);
+
+            Course course = dbConnection.courses.Find(id);
+   
             if (course == null)
             {
                 return HttpNotFound("Course not found");
+            }        
+            else
+            {
+                 dbConnection.Remove(course);
+                 dbConnection.SaveChanges();
+
+                 return RedirectToAction("CourseOverview");
             }
 
-            courses.Remove(course);
-            return RedirectToAction("CourseOverview");
         }
 
-        public ActionResult CourseOverview(int? selectedCourseId = null)
-        {
-            ViewBag.SelectedCourseId = selectedCourseId;
-            //ViewBag.CourseDTO = new CourseDTO(); //ved ikke
-            return View(courses);
-        }
         
         private static DateTime GetDayForWeekday(DateTime currentDate, DayOfWeek dayOfWeek)
         {
