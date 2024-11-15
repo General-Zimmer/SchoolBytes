6a11dcd790e84c2f9ec57419bb276e690279645f
nikojh9 <nikolaijuhl@gmail.com>
2024-11-15 09:21:29 +0100

    +oprettet participant controller

---
+++ SchoolBytes/SchoolBytes/Controllers/ParticipantController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolBytes.Models;
+
+namespace SchoolBytes.Controllers
+{
+    
+    public class ParticipantController : Controller
+    {
+        DBConnection dbConnection = DBConnection.getDBContext();
+
+        [HttpGet]
+        [Route("participant")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+    }
+}
