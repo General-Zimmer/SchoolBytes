9e0669549506a2f990d9fc210559988e46f24890
victoraae <73301911+victoraae@users.noreply.github.com>
2024-11-23 11:12:53 +0100

    isssue 40 wip 2

--- SchoolBytes/SpecFlowTests/StepDefinitions/TilmeldingStepDefinitions.cs
+++ SchoolBytes/SpecFlowTests/StepDefinitions/TilmeldingStepDefinitions.cs
@@ -12,9 +12,20 @@ namespace SpecFlowTests.StepDefinitions
         private static Participant bob = new Participant("Bob", "69695512");
         private static Teacher teacher1 = new Teacher() { Name = "teacher1", Id = 55435 };
         private static Course course1 = new Course() { Name = "Course1", Id = 666, Teacher = teacher1 };
-        private static CourseModule cm1 = new CourseModule() { Name = "cm1", Id = 888, Teacher = teacher1, Course = course1 };
+        private static CourseModule cm1 = new CourseModule() { Name = "cm1", Id = 888, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
+        private static CourseModule cmOutdated = new CourseModule() { Name="Outdated coursemodule", Id=62343, Teacher=teacher1, Course = course1, Date = DateTime.Now.AddDays(-1)};
+
+        private static CourseModule cm2 = new CourseModule() { Name = "cm2", Id = 123132232, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
+        private static CourseModule cm3 = new CourseModule() { Name = "cm3", Id = 423524, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
+        private static CourseModule cm4 = new CourseModule() { Name = "cm4", Id = 68368833, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
+        private static CourseModule cm5 = new CourseModule() { Name = "cm5", Id = 345372, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
+
+        private static CourseModule cm6 = new CourseModule() { Name = "cm6", Id = 323411114, Teacher = teacher1, Course = course1, Date = DateTime.Now.AddDays(1) };
 
         private Participant bobNonStatic = new Participant("Bob", "69695512");
+
+        private static Participant bobby = new Participant("Bobby", "A69695512");
+        private static Participant bob2 = new Participant("Bob2", "69695512") {Id=47284};
 
         [BeforeFeature]
         public static void BeforeTilmeldingFeature(FeatureContext featureContext)
@@ -61,91 +72,99 @@ namespace SpecFlowTests.StepDefinitions
         [Given(@"Participant Bob der er tilmeldt CourseModule cm1")]
         public void GivenParticipantBobDerErTilmeldtCourseModuleCm1()
         {
-            throw new PendingStepException();
+            //Se statiske felter og BeforeFeature øverst
         }
 
         [When(@"Bob bliver tilmeldt cm1 igen")]
         public void WhenBobBliverTilmeldtCm1Igen()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(666, 888, bob);
         }
 
         [Then(@"Er der kun en registration i DB")]
         public void ThenErDerKunEnRegistrationIDB()
         {
-            throw new PendingStepException();
+            DBConnection.GetSubscribeCount(bob).Should().Be(1);
         }
 
         [Given(@"Participant Bob og et CourseModule med forbigået dato")]
         public void GivenParticipantBobOgEtCourseModuleMedForbigaetDato()
         {
-            throw new PendingStepException();
+            //Se statiske felter og BeforeFeature øverst
         }
 
         [When(@"Bob bliver tilmeldt A")]
         public void WhenBobBliverTilmeldtA()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(666, cmOutdated.Id, bob);
         }
 
         [Then(@"Er der ikke en ny registration i DB A")]
         public void ThenErDerIkkeEnNyRegistrationIDBA()
         {
-            throw new PendingStepException();
+            _context.courseModules.Find(cmOutdated.Id).Registrations.Count().Should().Be(0);
         }
 
         [Given(@"Participant Bob med 5 aktive tilmeldte kurser")]
         public void GivenParticipantBobMed5AktiveTilmeldteKurser()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(course1.Id, cm2.Id, bob);
+            DBConnection.SubscribeTest(course1.Id, cm3.Id, bob);
+            DBConnection.SubscribeTest(course1.Id, cm4.Id, bob);
+            DBConnection.SubscribeTest(course1.Id, cm5.Id, bob);
         }
 
         [When(@"Bob bliver tilmeldt B")]
         public void WhenBobBliverTilmeldtB()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(course1.Id, cm6.Id, bob);
         }
 
         [Then(@"Er der ikke en ny registration i DB B")]
         public void ThenErDerIkkeEnNyRegistrationIDBB()
         {
-            throw new PendingStepException();
+            _context.courseModules.Find(cm6.Id).Registrations.Count().Should().Be(0);
+            DBConnection.GetSubscribeCount(bob).Should().Be(5);
         }
 
         [Given(@"Particpant Bobby med forkert format på properties")]
         public void GivenParticpantBobbyMedForkertFormatPaProperties()
         {
-            throw new PendingStepException();
+            //Se statiske felter og BeforeFeature øverst
         }
 
         [When(@"Bobby bliver tilmeldt")]
         public void WhenBobbyBliverTilmeldt()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(course1.Id, cm6.Id, bobby);
         }
 
         [Then(@"Er der ikk en ny registration i DB C")]
         public void ThenErDerIkkEnNyRegistrationIDBC()
         {
-            throw new PendingStepException();
+            _context.courseModules.Find(cm6.Id).Registrations.Count().Should().Be(0);
+            DBConnection.GetSubscribeCount(bobby).Should().Be(0);
+
         }
 
         [Given(@"Bob2 med samme telefonnummer som Bob")]
         public void GivenBob2MedSammeTelefonnummerSomBob()
         {
-            throw new PendingStepException();
+            //Se statiske felter og BeforeFeature øverst
         }
 
         [When(@"Bob2 bliver tilmeldt")]
         public void WhenBob2BliverTilmeldt()
         {
-            throw new PendingStepException();
+            DBConnection.SubscribeTest(course1.Id, cm6.Id, bob2);
         }
 
         [Then(@"Er der hverken en ny registration eller ny participant")]
         public void ThenErDerHverkenEnNyRegistrationEllerNyParticipant()
         {
-            throw new PendingStepException();
+            _context.courseModules.Find(cm6.Id).Registrations.Count().Should().Be(0);
+            DBConnection.GetSubscribeCount(bob2).Should().Be(0);
+            _context.participants.Find(bob2.Id).Should().Be(null);
         }
     }
 }
