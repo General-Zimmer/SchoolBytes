24fc211dc1be3af85cceb97cbd27e0fdf9a7fbc4
Kasper <kasperjonassen@yahoo.dk>
2024-11-25 12:12:57 +0100

    added one more test #8

---
+++ SchoolBytes/SchoolBytes/util/RemoveParticipantFromCourse.cs
@@ -0,0 +1,39 @@
+﻿using SchoolBytes.Models;
+using System;
+using System.Net;
+using System.Web.Mvc;
+
+namespace SchoolBytes.util
+{
+    public static class RemoveParticipantFromCourse
+    {
+        public static void DeleteParticipant(int participantId, int courseId, DBConnection dBConnection)
+        {
+            Course course = dBConnection.courses.Find(courseId);
+            Participant participant = dBConnection.participants.Find(participantId);
+
+            if (course == null)
+            {
+                Console.WriteLine("course not found");
+                //return new HttpStatusCodeResult(HttpStatusCode.NotFound, "course not found");
+            }
+            else if (participant == null)
+            {
+                Console.WriteLine("participant not found");
+                //return new HttpStatusCodeResult(HttpStatusCode.NotFound, "participant not found");
+            }
+            else if (!course.Participants.Contains(participant))
+            {
+                Console.WriteLine("participant not associated with course");
+                //return new HttpStatusCodeResult(HttpStatusCode.NotFound, "participant not associatied with course");
+            }
+            else
+            {
+                course.Participants.Remove(participant);
+                dBConnection.SaveChanges();
+
+                //return new HttpStatusCodeResult(HttpStatusCode.Redirect, "redireting to CourseOverview");
+            }
+        }
+    }
+}
--- SchoolBytes/SpecFlowTests/StepDefinitions/RemoveParticipantFromCourseStepDefinitions.cs
+++ SchoolBytes/SpecFlowTests/StepDefinitions/RemoveParticipantFromCourseStepDefinitions.cs
@@ -1,39 +1,66 @@
 using System;
+using System.IO;
 using TechTalk.SpecFlow;
+using SchoolBytes.Models;
+using SchoolBytes.util;
+using NUnit.Framework;
 
 namespace SpecFlowTests.StepDefinitions
 {
     [Binding]
     public class RemoveParticipantFromCourseStepDefinitions
     {
+        private static DBConnection context = DBConnection.getDBContext();
+        private static Participant mulan = new Participant("Mulan", "69695512");
+        private static Course course = new Course() { Name = "Course1", Id = 696 };
+
+
         [BeforeFeature]
         public static void Before()
         {
-            
+            context.Add(mulan);
+            context.Add(course);
+            course.Participants.Add(mulan);
+            context.SaveChanges();
         }
 
         [AfterFeature]
-        public static void After() 
-        { 
-
+        public static void After()
+        {
+            context.Remove(course);
+            context.Remove(mulan);
+            context.SaveChanges();
         }
 
         [Given(@"\[Participant is enrolled to course]")]
         public void GivenParticipantIsEnrolledToCourse()
         {
-            throw new PendingStepException();
+            course.Participants.Should().Contain(mulan);
         }
 
         [When(@"\[When user removes participant from course]")]
         public void WhenWhenUserRemovesParticipantFromCourse()
         {
-            throw new PendingStepException();
+            RemoveParticipantFromCourse.DeleteParticipant(mulan.Id, course.Id, context);
         }
 
         [Then(@"\[Participant should be removed from the list in course]")]
         public void ThenParticipantShouldBeRemovedFromTheListInCourse()
         {
-            throw new PendingStepException();
+            course.Participants.Should().NotContain(mulan);
+        }
+
+        [Then(@"\[Show error message that partcipant is not in course anymore]")]
+        public void ThenShowErrorMessageThatPartcipantIsNotInCourseAnymore()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            RemoveParticipantFromCourse.DeleteParticipant(mulan.Id, course.Id, context);
+            string consoleOutput = stringWriter.ToString(); 
+            Assert.That(consoleOutput, Does.Contain("participant not associated with course"));
+
         }
     }
 }
+
