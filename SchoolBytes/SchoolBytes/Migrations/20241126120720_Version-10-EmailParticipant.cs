1977c5a6e2d0e17e0a0009b515b7e66efaa383ac
Denduff <Andreas.elbyhoej@live.dk>
2024-11-26 13:07:38 +0100

    merged main and fixed minor stuff

---
+++ SchoolBytes/SchoolBytes/Migrations/20241126120720_Version-10-EmailParticipant.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolBytes.Migrations
+{
+    public partial class Version10EmailParticipant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "participants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "participants");
+        }
+    }
+}
